5c1ad106d0824a70d9e21d4cb68bb64dafc65d75
danielx285 <daniel.carlos.melo@ccc.ufcg.edu.br>
2019-02-20 01:45:58 -0300

    Organizando Tudo pra começar a implementar coisas novas

--- Cadastrar_Users/flaskk.py
+++ Cadastrar_Users/flaskk.py
@@ -1,38 +1,53 @@
-#coding: utf-8
+# coding: utf-8
 import bd
-from flask import Flask, abort
+from flask import Flask, abort, render_template, url_for
 
 app = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-	html = ['<ul>']
-	for username, user in bd.users.items():
-		html.append(
-			"<li><a href='/user/%s'>%s</a></li>" \
-			% (username, user["name"])
-		)
-	html.append("</ul>")
-	return "\n".join(html)
+    return render_template("home.html")
+
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+
+@app.route("/users")
+def users():
+    html = ['<ul>']
+    for username, user in bd.users.items():
+        html.append(
+            "<li><a href='/user/%s'>%s</a></li>" \
+            % (username, user["name"])
+        )
+    html.append("</ul>")
+    return "\n".join(html)
+
 
 def profile(username):
-	user = bd.users.get(username)
+    user = bd.users.get(username)
 
-	if user:
-		html_code = "<h1>%s</h1>" % user["name"]  \
-			+ "\n<img src='%s' /><br/>" % user["imagen"]  \
-			+ "\ntelefone: %s <br/>" % user["tel"] \
-			+ "\n<a href='/'>Voltar<a/>"
-		return html_code
-	else :
-		return abort(404, "User not found")
+    if user:
+        html_code = "<h1>%s</h1>" % user["name"] \
+                    + "\n<img src='%s' width = '200px' /><br/>" % user["imagen"] \
+                    + "\ntelefone: %s <br/>" % user["tel"] \
+                    + "\n<a href='/'>Voltar<a/>"
+        return html_code
+    else:
+        return abort(404, "User not found")
+
 
 app.add_url_rule('/user/<username>/', view_func=profile, endpoint='user')
 
+
 @app.route("/about")
 def about():
-	return "<h1>Esta é uma simples página criada para que eu possa aprender sobre programação Web</h1>"
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
